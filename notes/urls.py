10c8c1ec303986550d82adbff0e76995e6319707
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-03-07 15:41:23 +0300

    add url for note objects using pk

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes/', views.list, name='list'),
-    
+    path('not3es/', views.list, name='list'),
+    path('notes/<int:pk>', views.detail, name='detail'),
 ]
